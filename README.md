56c27b306f119dc2b065f971a0710f0298a7b91e
Y.Nanthachai <thecoachingonline@gmail.com>
2023-04-17 06:38:30 +0000

    Device

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Hello-Quantum-AWS-Braket
 Hello World Amazon Quantum Braket
+
+pip install amazon-braket-sdk
--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 import boto3
+from braket.aws import AwsDevice
 from braket.circuits import Circuit
 from braket.devices import LocalSimulator
+
+device = AwsDevice("arn:aws:braket:::device/quantum-simulator/amazon/sv1")
 
 # Create a circuit with one qubit
 circuit = Circuit().h(0).measure(0)
